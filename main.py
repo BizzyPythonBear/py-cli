0960f8d9a2d1c5307aff64b0139032e470a190f4
BizzyPythonBear <bizzeebear@hotmail.com>
2022-01-25 20:38:49 -0800

    added some testing stuff

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-import typing
+from typing import Sequence
+import sys
+import argparse
 
 def error(error: str, error_line: int):
-    print(f"{error} | Error on line {str(error_line)}")
-    s = []
+    print(f"{error} | Error on line: {str(error_line)}")
     for i in error:
-        print("^", end ="")
+        print("^", end="")
+
+def parse_args(arg: str) -> bool:
+    lower_arg = arg.lower()
+
+    # ILL USE ARG PARSE SOON
+    if lower_arg == 'foo':
+        return True
+    else:
+        error(arg, 1)
+
+def main() -> int:
+    args_len = len(sys.argv)
+    argument_1 = args_len - 1
+    if parse_args(sys.argv[int(argument_1)]):
+        print("bar")
+    return 1
+
+if __name__ == '__main__':
+    raise SystemExit(main())
