bb91d612f8de0d269ad1b15107aa5c60ee2039f6
BizzyPythonBear <bizzeebear@hotmail.com>
2022-01-26 18:40:42 -0800

    Now prints to stderr

--- utils/error.py
+++ utils/error.py
@@ -1,9 +1,11 @@
+import sys
+
 def error(error: str, error_line: int, type: str):
     if type == 'str':
-        print(f"{error} | Error on line: {str(error_line)} | Not a string")
+        print(f"{error} | Error on line: {str(error_line)} | Not a string", file=sys.stderr)
     elif type == 'int':
-        print(f"{error} | Error on line: {str(error_line)} | Not a integer")
+        print(f"{error} | Error on line: {str(error_line)} | Not a integer", file=sys.stderr)
     elif type == 'file':
-        print(f"{error} | Error on line: {str(error_line)} | File doesn't exist")
+        print(f"{error} | Error on line: {str(error_line)} | File doesn't exist", file=sys.stderr)
     for i in error:
         print("^", end="")
